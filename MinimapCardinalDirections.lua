bf3c67e918458de2722bfd62fe65319774e95c71
Jesper Brown <jesper.brown@edgeware.tv>
2019-08-08 12:08:43 +0200

    Initial creation

---
+++ MinimapCardinalDirections.lua
@@ -0,0 +1,14 @@
+Minimap:CreateFontString("CardinalDirectionWest","OVERLAY","NumberFontNormalSmall");
+CardinalDirectionWest:SetPoint("LEFT",0,0);
+CardinalDirectionWest:SetTextColor(1,0.875,0,0.625);
+CardinalDirectionWest:SetText("W");
+
+Minimap:CreateFontString("CardinalDirectionEast","OVERLAY","NumberFontNormalSmall");
+CardinalDirectionEast:SetPoint("RIGHT",0,0);
+CardinalDirectionEast:SetTextColor(1,0.875,0,0.625);
+CardinalDirectionEast:SetText("E");
+
+Minimap:CreateFontString("CardinalDirectionSouth","OVERLAY","NumberFontNormalSmall");
+CardinalDirectionEast:SetPoint("BOTTOM",0,0);
+CardinalDirectionEast:SetTextColor(1,0.875,0,0.625);
+CardinalDirectionEast:SetText("S");
