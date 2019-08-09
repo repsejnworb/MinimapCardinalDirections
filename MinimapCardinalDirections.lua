d43836833937c7d3ce1ddc7be2883796c5e53c54
Jesper Brown <jesper.brown@edgeware.tv>
2019-08-09 12:03:31 +0200

    Updated to look better

--- MinimapCardinalDirections.lua
+++ MinimapCardinalDirections.lua
@@ -1,14 +1,14 @@
-Minimap:CreateFontString("CardinalDirectionWest","OVERLAY","NumberFontNormalSmall");
-CardinalDirectionWest:SetPoint("LEFT",0,0);
+Minimap:CreateFontString("CardinalDirectionWest","OVERLAY","NumberFontNormalYellow");
+CardinalDirectionWest:SetPoint("LEFT",5,0);
 CardinalDirectionWest:SetTextColor(1,0.875,0,0.625);
 CardinalDirectionWest:SetText("W");
 
-Minimap:CreateFontString("CardinalDirectionEast","OVERLAY","NumberFontNormalSmall");
-CardinalDirectionEast:SetPoint("RIGHT",0,0);
+Minimap:CreateFontString("CardinalDirectionEast","OVERLAY","NumberFontNormalYellow");
+CardinalDirectionEast:SetPoint("RIGHT",-5,0);
 CardinalDirectionEast:SetTextColor(1,0.875,0,0.625);
 CardinalDirectionEast:SetText("E");
 
-Minimap:CreateFontString("CardinalDirectionSouth","OVERLAY","NumberFontNormalSmall");
-CardinalDirectionSouth:SetPoint("BOTTOM",0,0);
+Minimap:CreateFontString("CardinalDirectionSouth","OVERLAY","NumberFontNormalYellow");
+CardinalDirectionSouth:SetPoint("BOTTOM",0,5);
 CardinalDirectionSouth:SetTextColor(1,0.875,0,0.625);
 CardinalDirectionSouth:SetText("S");
